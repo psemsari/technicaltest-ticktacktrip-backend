2ca23b64a3ae662ba99ca8ff0190fdcf0b91adb4
Pierre SN <41895689+psemsari@users.noreply.github.com>
2023-06-21 23:56:31 +0000

    fix: ip adress and port in var on main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {rawBody: true});
   app.useBodyParser('text')
-  await app.listen(1000);
+  await app.listen(process.env.PORT || 1000, process.env.HOST || 'localhost');
 }
 bootstrap();
