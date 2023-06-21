af9acfe4766df3ec9af00d7316323f32ed424c87
Pierre SN <41895689+psemsari@users.noreply.github.com>
2023-06-21 14:27:05 +0000

    fix :  change to env port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {rawBody: true});
   app.useBodyParser('text')
-  await app.listen(80);
+  await app.listen(port);
 }
 bootstrap();
