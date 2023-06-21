7e960845b1a6faa6fc87b9138fdea7a815bba5c5
Pierre SN <41895689+psemsari@users.noreply.github.com>
2023-06-21 16:08:02 +0000

    minor changes for test

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 3000;
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {rawBody: true});
   app.useBodyParser('text')
   console.log('Application is running on : ' + app.getUrl());
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
