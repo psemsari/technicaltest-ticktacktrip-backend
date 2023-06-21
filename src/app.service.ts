8fa7bc7bd897cca9431210b09cb5c42eb03b5a95
Pierre SN <41895689+psemsari@users.noreply.github.com>
2023-06-21 11:18:27 +0000

    justify text end

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,53 +1,79 @@
 import { Injectable } from '@nestjs/common';
 
-// build Markov Chain data structures;
-// while( more words to generate ) {
-//    generate next word;
-//    if( word is short enough to fit on current output line )
-//       add word and trailing space(s) to the line;
-//          // Two spaces if it is the end of a sentence.  See below.
-//    else {
-//       add spaces to justify the line; // details in phase 3
-//       print the line;
-//       clear the linked list;
-//       add the word and trailing spaces to the line;
-//    }
-// }
-// if( output line is not emtpy )
-//    print output line;
+function addWordToResult(res: string[], buf: string[], maxWidth: number) {
+  // How many spaces are needed
+  let spaces :number = maxWidth - buf.reduce((acc, cur) => cur.length + acc, 0);
+  
+  // If there is only one word on the line
+  // then just add the padding to the end and return
+  if (buf.length === 1) {
+      buf[0] += ' '.repeat(spaces);
+      res.push(buf[0]);
+      return;
+  }
+  
+  // If the line has more than one word,
+  // decrement the spaces which are created during the buf.join(' ')
+  // which is n - 1 spaces
+  spaces -= buf.length - 1;
+  
+  // All words except the last should have spaces added to their string
+  const end = buf.length - 1;
+  let index = 0;
+  
+  // If there are spaces to distribute, distribute them
+  // evenly looping back to the beginning of the buffer
+  while (spaces-- > 0) {
+      buf[index] += ' ';
+      
+      index = (index + 1) % end;
+  }
+  
+  res.push(buf.join(' '))
+}
 
 function justifyText(text: string, lineLength: number): string {
 
-  const words = text.split(' ');
-  const lines: string[] = [];
-  let currentLineLenght: number = 0;
-  let spaceNeeded: number = 0;
-  let finalText = '';
+  const words: string[] = text.split(' ');
+  const res: string[] = [];
+  let buf: string[] = [];
+  let width: number = lineLength;
 
-  for (const word of words) {
-    if (currentLineLenght + word.length + 1 <= lineLength) {
-      lines.push(word);
-      currentLineLenght += word.length + 1
+  words.forEach(word => {
+    // Check if the word fits in the current line
+    // A word fits if theres enough room for the word and
+    // a space between it and the word to the left
+    if (word.length <= (width - buf.length)) {
+        buf.push(word);
+        width -= word.length;
     } else {
-      spaceNeeded = lineLength - currentLineLenght
-      finalText += justifyLine(lines, spaceNeeded)
+        // The word did not fit on the line, send this line for padding
+        addWordToResult(res, buf.slice(), lineLength);
+        
+        // Start a new line with the current word
+        buf = [word];
+        // reset the current line width
+        width = lineLength - word.length;
     }
+  });
+
+  // This is the final lines processing
+  // According the rules this should only be left justified
+  // so add all padding to the right not between the words
+  if (buf.length) {
+      let str = buf.join(' ');
+      str += ' '.repeat(lineLength - str.length);
+      res.push(str);
   }
 
-  return finalText
+  let finalresult : string = '';
 
-}
+  res.forEach((value) => {
+    finalresult += value + '\n'
+  })
 
-function justifyLine(words: string[], spaceNeeded: number) {
-  let finalLine = ''
-  const spacePerWord = words.length / spaceNeeded
-  
-  for (const word of words) {
-    finalLine += word + ' '.repeat(Math.floor(spaceNeeded / spacePerWord))
-    spaceNeeded -= spacePerWord
-  }
+  return finalresult;
 
-  return finalLine
 }
 
 @Injectable()
@@ -58,10 +84,6 @@ export class AppService {
     const justifiedText = justifyText(text, widthtext);
 
     return justifiedText;
-  }
-
-  getToken(): string {
-    return 'Hello World!';
   }
 }
 
