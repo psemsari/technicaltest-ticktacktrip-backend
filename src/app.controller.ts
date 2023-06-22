a9591175d46d74cda1a4e253ef7231d2708dc928
Pierre SN <41895689+psemsari@users.noreply.github.com>
2023-06-22 12:30:14 +0000

    feat: add swagger documentation /api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,22 +13,43 @@ import { AppService } from './app.service';
 
 import { AuthGuard } from './auth/auth.guard';
 import { RateLimiterMemory } from 'rate-limiter-flexible';
+import {
+  ApiTags,
+  ApiSecurity,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiMethodNotAllowedResponse,
+  ApiProduces,
+  ApiResponse, ApiConsumes, ApiHeader, ApiUnauthorizedResponse, ApiBody
+} from '@nestjs/swagger';
+import { readFileSync } from 'fs';
 
 const limiter = new RateLimiterMemory({
   points: 80000, // Nombre total de mots autorisés par jour
   duration: 86400, // Durée en secondes (1 jour = 24 * 60 * 60)
 });
 
+@ApiTags('Justify')
+@ApiSecurity('bearer')
+@ApiBearerAuth()
 @Controller("api")
 export class AppController {
   constructor(private readonly appService: AppService) {}
   
+  @ApiProduces('text/plain')
+  @ApiConsumes('text/plain')
+  @ApiBody({type: String,
+  examples: {"test text": {value: readFileSync('../input.txt', 'utf-8')}}})
+  @ApiOkResponse({description: "return justify the content", type: String})
+  @ApiMethodNotAllowedResponse({description: "need content-type to text/plain"})
+  @ApiUnauthorizedResponse({description: "need a bearer token from /api/token"})
+  @ApiResponse({ status: 402, description: 'you have exceeded the 80,000-word limit'})
+
   @UseGuards(AuthGuard)
   @Post("justify")
   @Header("Content-Type", 'text/plain')
   @HttpCode(200)
   async justify(
-    @Headers("Content-Type") type: string,
     @Req() req)
     : Promise<string> {
       const text: string = req.rawBody.toString();
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,27 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiBadRequestResponse, ApiBody, ApiConsumes, ApiOkResponse, ApiParam, ApiProduces, ApiProperty, ApiTags } from '@nestjs/swagger';
 
+class bearer {
+  @ApiProperty()
+  bearer: string
+}
+
+class mail {
+  @ApiProperty()
+  email: string
+}
+
+@ApiTags('Security')
 @Controller('api')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @ApiBody({description: "need an email for authentification", type: mail, examples: {"To Use": {value: '{"email":"foo@bar.com"}'}}})
+  @ApiConsumes('application/json')
+  @ApiProduces('application/json')
+  @ApiOkResponse({description: "return bearer token", type: bearer})
+  @ApiBadRequestResponse({description: "user unknown"})
 
   @HttpCode(HttpStatus.OK)
   @Post('token')
