fe79598b6e816c92f77bdfaf246715b71d2e7217
Pierre SN <41895689+psemsari@users.noreply.github.com>
2023-06-22 13:35:41 +0000

    fix

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT, process.env.HOST); 
+  await app.listen(process.env.PORT, process.env.HOST);
 }
 bootstrap();
