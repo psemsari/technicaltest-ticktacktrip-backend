43a393a5e9a96f085c875b4090492d452fadb3a1
Pierre SN <41895689+psemsari@users.noreply.github.com>
2023-06-22 00:01:59 +0000

    repush for vercel test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {rawBody: true});
   app.useBodyParser('text')
-  await app.listen(process.env.PORT || 1000, process.env.HOST || 'localhost');
+  await app.listen(process.env.PORT || 1000, process.env.HOST || 'localhost'); 
 }
 bootstrap();
